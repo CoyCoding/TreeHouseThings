76a875f3c5e66d7c9b51af9daa7b57f6929226f1
Matt Coy <40921499+fluffytoycoy@users.noreply.github.com>
2018-12-15 12:51:07 -0600

    Create SplitExtension.cs

---
+++ SplitExtension.cs
@@ -0,0 +1,13 @@
+public static class StringExtensions
+{
+    public static string[] Split(this string @this, char separator, int count)
+    {
+      ` if(@this = null)
+        {
+            throw new ArgumentNullException(nameof(@this));
+        }
+        
+        return @this.Split(new [] {separator}, count);
+        
+    }
+}
